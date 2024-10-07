14ed980380ebd8c8498bb59f99c0ce5b7ab24df4
lennox <lennoxngubo549@gmail.com>
2024-10-07 11:51:47 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+const superbowlWin = (array) => {
+    let resultobj = array.find(obj => obj.result === `W`)
+    if (resultobj) {
+        return resultobj.year
+    }else return undefined
+}
